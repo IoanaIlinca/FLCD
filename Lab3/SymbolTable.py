05554957e7e8aa3f6fdcd325fa67bc8bb2d2adb2
Moldovan Ioana Ilinca <miie2774@scs.ubbcluj.ro>
2021-10-12 14:08:30 +0300

    first 3 labs

---
+++ Lab3/SymbolTable.py
@@ -0,0 +1,55 @@
+class SymbolTable:
+
+    def __init__(self):
+        self.__hashTable = {}
+        self.__firstEmpty = 0
+
+    def add(self, value):
+        initialValue = value
+        value = str(value)
+        sum = 0
+        for character in value:
+            sum += ord(character)
+
+        if sum not in self.__hashTable.keys():
+            self.__hashTable[sum] = [initialValue, -1]
+            if sum == self.__firstEmpty:
+                while self.__firstEmpty in self.__hashTable.keys():
+                    self.__firstEmpty += 1
+        else:
+            if initialValue not in self.__hashTable[sum]:
+                lastElement = ['somevalue', sum]
+                element = self.__hashTable[sum]
+                if (initialValue in self.__hashTable[sum]):
+                    return (sum, initialValue)
+                while element[1] != -1:
+                    lastElement = element
+                    element = self.__hashTable[element[1]]
+                    if (initialValue in self.__hashTable[lastElement]):
+                        return (lastElement, initialValue)
+
+                print(self.__hashTable[lastElement[1]])
+                self.__hashTable[lastElement[1]][1] = self.__firstEmpty
+                self.__hashTable[self.__firstEmpty] = [initialValue, -1]
+                sum = self.__firstEmpty
+                while self.__firstEmpty in self.__hashTable.keys():
+                    self.__firstEmpty += 1
+
+        return (sum, initialValue)
+
+    def getContent(self):
+        return self.__hashTable
+
+
+if __name__ == "__main__":
+    symTab = SymbolTable()
+
+    print(symTab.add(1))
+    print(symTab.add('abc'))
+    print(symTab.add('anaaremere'))
+    print(symTab.add(2))
+    print(symTab.add(1))
+    print(symTab.add('abc'))
+    print(symTab.add('acb'))
+    print(symTab.add('bca'))
+    print(symTab.getContent())
